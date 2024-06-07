1198673bfeb2fd5ee8be06729dc9f4fee96f96f6
Youda Zhou <youda.zhou@accenture.com>
2024-06-07 11:27:08 +0200

    Initial Commit

---
+++ Virtual TouristUITests/Virtual_TouristUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Virtual_TouristUITestsLaunchTests.swift
+//  Virtual TouristUITests
+//
+//  Created by Youda Zhou on 7/6/24.
+//
+
+import XCTest
+
+final class Virtual_TouristUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
